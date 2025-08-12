37846efc97847cb008f2732db20357454201d9bd
JproCreator <elpoderdelpro13@gmail.com>
2025-08-11 20:46:41 -0500

    HolaMundo

---
+++ LogícaDeProgramación/HolaMundo.java
@@ -0,0 +1,14 @@
+// Clase del mismo nombre del fichero "HolaMundo"
+public class HolaMundo {
+
+// Operación para ejecutar el programa
+    public static void main(String[] args){ 
+        //Sintaxis inicial para indicar donde se va ejecutar el programa
+        // Método "main" public=público;static=ejecución; void=no retorno
+
+        System.out.println("Hola, Java")
+        // System; out=salida; println=
+    }
+    
+}
+
